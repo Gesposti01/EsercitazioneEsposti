587c68c1bdd956000f39a0ec34235ca9bb42090c
xamarin <xamarin@rosselli.loc>
2019-12-19 12:22:42 +0100

    Aggiungere i file di progetto.

---
+++ EsercitazioneEsposti/EsercitazioneEsposti/constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EsercitazioneEsposti
+{
+    class constants
+    {
+        
+        public const string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+        public const string OpenWeatherMapAPIKey = "e9d1f4485ac30cd6876a08572bfb5f4f";
+    }
+
+}
